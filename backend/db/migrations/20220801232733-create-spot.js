968486d1fb105d7e90fc77cc4a3e043fbc7c885f
Katy Kam <katyky14@gmail.com>
2022-08-02 11:47:30 -0700

    get spots of current user

--- backend/db/migrations/20220801232733-create-spot.js
+++ backend/db/migrations/20220801232733-create-spot.js
@@ -11,8 +11,6 @@ module.exports = {
       ownerId: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        references: { model: 'Users'},
-        onDelete: 'CASCADE'
       },
       address: {
         type: Sequelize.STRING,
