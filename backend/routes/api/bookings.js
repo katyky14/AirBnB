3f9e828bbf25917fa122871286eadd51f05dd9bb
Katy Kam <katyky14@gmail.com>
2022-08-04 15:54:20 -0700

    edit a booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -44,7 +44,57 @@ router.get('/current', requireAuth, async (req, res) => {
 
 //EDIT A BOOKING
 router.put('/:bookingId', requireAuth ,async (req, res) => {
-    
+    const { bookingId } = req.params;
+
+    const { startDate, endDate } = req.body;
+
+    const booked = await Booking.findByPk(bookingId);
+
+    if(!booked) {
+        return res.json({
+            "message": "Booking couldn't be found",
+            "statusCode": 404
+        })
+    }
+
+
+      // before or on startDate
+      let todayDate = new Date().toISOString().slice(0, 10)
+
+      if (endDate < todayDate || endDate < startDate || startDate < todayDate) {
+              res.json({
+                "message": "Past bookings can't be modified",
+                "statusCode": 403
+              })
+      };
+
+      // conflicted dates
+      const dates = await Booking.findAll({
+          where: {
+              [Op.and]: [
+                  { startDate: startDate },
+                  { spotId: spotId }
+              ]
+          }
+      });
+
+      if (dates.length >= 1) {
+        res.json({
+            "message": "Sorry, this spot is already booked for the specified dates",
+            "statusCode": 403,
+            "errors": {
+                "startDate": "Start date conflicts with an existing booking",
+                "endDate": "End date conflicts with an existing booking"
+            }
+        })
+    }
+
+
+    booked.startDate =  startDate;
+    booked.endDate = endDate;
+    await booked.save();
+
+    res.json(booked)
 })
 
 
